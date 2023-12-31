05e580f970dd7df58a619f2bb9848eeb478d7111
ylpoonlg <poon.yat.long@gmail.com>
2023-12-31 17:01:31 +0000

    Change version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1575
-val canonicalVersionName = "7.53.3"
+val canonicalVersionName = "7.53.3-lg.0"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
